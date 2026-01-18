60cdc3a7c1697fc843d34b10c8b757107243bd86
Konan Kramo <fabriceemmanuel2@gmail.com>
2026-01-18 13:09:43 +0100

    modif de slreamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import tensorflow as tf
+import numpy as np
 
-st.title("Mon modèle TensorFlow")
-st.write("Bienvenue sur mon interface Streamlit.")
+st.title("Interface de test du modèle")
+
+model = tf.keras.models.load_model("mon_modele_ia.h5")
+
+value = st.number_input("Entrer une valeur")
+
+if st.button("Prédire"):
+    arr = np.array([[value]], dtype=float)
+    pred = model.predict(arr)
+    st.write("Résultat :", pred.tolist())
